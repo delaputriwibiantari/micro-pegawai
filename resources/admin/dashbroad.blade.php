7920f211487bda3a4165c853dbf682e66e11c474
finarifdania <finarifdania1905@gmail.com>
2025-10-20 14:34:59 +0700

    feat : tambah admin person

---
+++ resources/admin/dashbroad.blade.php
@@ -0,0 +1,14 @@
+@extends('admin.layout.index')
+
+@section('css')
+@endsection
+
+@section('content')
+    <div class="container-fluid">
+
+    </div>
+@endsection
+
+@section('javascript')
+@endsection
+
