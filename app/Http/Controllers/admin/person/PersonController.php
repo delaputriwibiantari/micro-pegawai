59f036b0fde9b435c477c50045bd9969ea7512b7
dela wibiantari <delawibiantari10@gmail.com>
2025-10-26 11:09:22 +0700

    feat : controller request service dan model person

--- app/Http/Controllers/admin/person/PersonController.php
+++ app/Http/Controllers/admin/person/PersonController.php
@@ -2,14 +2,14 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Person\Person;
+use App\Http\Requests\Person\PersonStoreRequest;
+use App\Http\Requests\Person\PersonUpdateRequest;
 use App\Services\Person\PersonService;
 use App\Services\Tools\ResponseService;
 use App\Services\Tools\TransactionService;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Request;
-use Illuminate\Support\Js;
+
 
 class PersonController extends Controller
 {
@@ -44,6 +44,92 @@ class PersonController extends Controller
         return $this->transactionService->handleWithDataTable(
             fn()=> $this->personService->getListData()
         );
+    }
+
+    public function store(PersonStoreRequest $request): JsonResponse
+    {
+        $foto = $request->file('foto');
+
+        return $this->transactionService->handleWithTransaction(function () use ($request, $foto) {
+            $payload = $request->only([
+                'nama_lengkap',
+                'nama_panggilan',
+                'tempat_lahir',
+                'tanggal_lahir',
+                'agama',
+                'kewarganegaraan',
+                'email',
+                'no_hp',
+                'nik',
+                'kk',
+                'npwp',
+                'alamat',
+                'id_desa',
+                'jk',
+                'golongan_darah',
+                'rt',
+                'rw',
+            ]);
+
+            $created = $this->personService->create($payload);
+
+            if ($foto) {
+                $uploadResult = $this->personService->handleFileUpload($foto);
+                $created->update(['foto' => $uploadResult['file_name']]);
+            }
+
+            return $this->responseService->successResponse('Data berhasil dibuat', $created, 201);
+        });
+    }
+
+    public function update(PersonUpdateRequest $request, string $id): JsonResponse
+    {
+        $data = $this->personService->findById($id);
+        if (!$data) {
+            return $this->responseService->errorResponse('Data tidak ditemukan');
+        }
+
+        $foto = $request->file('foto');
+
+        return $this->transactionService->handleWithTransaction(function () use ($request, $data, $foto) {
+            $payload = $request->only([
+                'nama_lengkap',
+                'nama_panggilan',
+                'tempat_lahir',
+                'tanggal_lahir',
+                'agama',
+                'kewarganegaraan',
+                'email',
+                'no_hp',
+                'nik',
+                'kk',
+                'npwp',
+                'alamat',
+                'id_desa',
+                'jk',
+                'golongan_darah',
+                'rt',
+                'rw',
+            ]);
+
+            $updatedData = $this->personService->update($data, $payload);
+
+            if ($foto) {
+                $uploadResult = $this->personService->handleFileUpload($foto, $updatedData);
+                $updatedData->update(['foto' => $uploadResult['file_name']]);
+            }
+
+            return $this->responseService->successResponse('Data berhasil diperbarui', $updatedData);
+        });
+    }
+
+    public function show(string $id): JsonResponse
+    {
+        return $this->transactionService->handleWithShow(function () use ($id) {
+            $data = $this->personService->getDetailData($id);
+
+            return $this->responseService->successResponse('Data berhasil diambil', $data);
+        });
     }
 
 }
