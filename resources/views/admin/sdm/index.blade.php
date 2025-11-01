2e19650c07ef852603fe1f4665b40f22aa88b195
finarifdania <finarifdania1905@gmail.com>
2025-11-01 17:10:27 +0700

    fix: index dan sdm

--- resources/views/admin/sdm/index.blade.php
+++ resources/views/admin/sdm/index.blade.php
@@ -1,58 +1,53 @@
 @extends('admin.layout.index')
 
 @section('css')
-    <link rel="stylesheet" href="{{ asset('assets/plugins/datatables/dataTables.bootstrap5.min.css') }}"/>
-    <link rel="stylesheet" href="{{ asset('assets/plugins/datatables/responsive.bootstrap.min.css') }}"/>
-    <link rel="stylesheet" href="{{ asset('assets/plugins/datatables/buttons.dataTables.min.css') }}"/>
-@endsection
-@section('list')
-    <li class="breadcrumb-item text-muted">SDM</li>
-    <li class="breadcrumb-item">
-        <span class="bullet bg-gray-200 w-5px h-2px"></span>
-    </li>
-    <li class="breadcrumb-item text-dark">Data SDM</li>
+    <!-- DataTables CSS -->
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/dataTables.bootstrap5.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/responsive/2.5.0/css/responsive.bootstrap5.min.css">
 @endsection
 
 @section('content')
     <div class="container-fluid">
-
         <div class="content flex-column-fluid">
             <div class="card mb-xl-8 mb-5 border-2 shadow">
                 <div class="card-header">
                     <h3 class="card-title align-items-start flex-column">
-                        <span class="card-label fw-bolder mb-1 ">Data SDM</span>
+                        <span class="card-label fw-bolder mb-1">Data SDM</span>
                     </h3>
                     <div class="card-toolbar">
                         <div class="d-flex justify-content-end" data-kt-user-table-toolbar="base">
                             <a type="button" class="btn btn-sm btn-primary fs-sm-8 fs-lg-6" data-bs-toggle="modal"
-                               data-bs-target="#form_create" title="Tambah Person">Tambah SDM</a>
+                               data-bs-target="#form_create" title="Tambah SDM">Tambah SDM</a>
                         </div>
                     </div>
                 </div>
                 <div class="card-body p-5">
+                    <!-- Notice Box seperti di Person -->
                     <div class="notice d-flex border-primary mb-4 rounded border border-dashed p-4 shadow bg-hover-light-dark">
                         <div class="d-flex flex-stack fs-sm-8 fs-lg-6">
                             <div class="row">
-                                <span class="text-gray-700">Berikut ini adalah data SDM.</span>
+                                <span class="text-gray-700">Berikut ini adalah data SDM (Sumber Daya Manusia).</span>
                             </div>
                         </div>
                     </div>
+
+                    <!-- Table Container dengan styling yang sama -->
                     <div class="table-responsive mb-8 shadow p-4 mx-0 border-hover-dark border-primary border-1 border-dashed fs-sm-8 fs-lg-6 rounded-2">
                         <div class="table-responsive">
-                            <table id="example"
-                                   class="table table-sm align-middle table-row-bordered table-row-solid gs-0 gy-2">
+                            <table id="sdmTable" class="table table-sm align-middle table-row-bordered table-row-solid gs-0 gy-2">
                                 <thead>
-                                <tr class="text-start text-gray-400 fw-bold fs-7 text-uppercase gs-0 fs-sm-8 fs-lg-6">
-                                    <th class="min-w-75px ps-5">Aksi</th>
-                                    <th class="min-w-150px">Nip</th>
-                                    <th class="min-w-60px">Status Pegawai</th>
-                                    <th class="min-w-120px">Tipe Pegawai</th>
-                                    <th class="min-w-100px">Tanggal Masuk</th>
-                                    <th class="min-w-120px">Id Person</th>
-                                    <th class="min-w-120px">Nama Lengkap</th>
-                                </tr>
+                                    <tr class="text-start text-gray-400 fw-bold fs-7 text-uppercase gs-0 fs-sm-8 fs-lg-6">
+                                        <th class="min-w-75px ps-5">Aksi</th>
+                                        <th class="min-w-150px">NIP</th>
+                                        <th class="min-w-60px">Status Pegawai</th>
+                                        <th class="min-w-120px">Tipe Pegawai</th>
+                                        <th class="min-w-100px">Tanggal Masuk</th>
+                                        <th class="min-w-120px">ID Person</th>
+                                        <th class="min-w-120px">Nama Lengkap</th>
+                                    </tr>
                                 </thead>
                                 <tbody class="text-gray-800 fw-bolder fs-sm-8 fs-lg-6">
+                                    <!-- Data akan diisi oleh DataTables -->
                                 </tbody>
                             </table>
                         </div>
@@ -61,26 +56,30 @@
             </div>
         </div>
     </div>
-    @include('admin.sdm.view.detail')
     @include('admin.sdm.view.create')
     @include('admin.sdm.view.edit')
+    @include('admin.sdm.view.detail')
 @endsection
 
 @section('javascript')
-    <script src="{{ asset('assets/plugins/datatables/jquery.dataTables.min.js') }}"></script>
-    <script src="{{ asset('assets/plugins/datatables/lodash.min.js') }}"></script>
-    <script src="{{ asset('assets/plugins/datatables/dataTables.bootstrap5.min.js') }}"></script>
-    <script src="{{ asset('assets/plugins/datatables/dataTables.colReorder.min.js') }}"></script>
-    <script src="{{ asset('assets/plugins/datatables/dataTables.responsive.min.js') }}"></script>
-    <script src="{{ asset('assets/plugins/datatables/dataTables.buttons.min.js') }}"></script>
+    @include('admin.sdm.script.list')
+    @include('admin.sdm.script.create')
+    @include('admin.sdm.script.edit')
+    @include('admin.sdm.script.detail')
+<!-- JQUERY -->
+<script src="https://code.jquery.com/jquery-3.7.0.min.js"></script>
 
-    <script src="{{ asset('assets/plugins/datatables/vfs_fonts.js') }}"></script>
-    <script src="{{ asset('assets/plugins/datatables/buttons.html5.min.js') }}"></script>
-    <script src="{{ asset('assets/plugins/datatables/jszip.min.js') }}"></script>
-    <script src="{{ asset('assets/plugins/datatables/buttons.colVis.min.js') }}"></script>
-    <script src="{{ asset('assets/plugins/datatables/print.js') }}"></script>
-    <script src="{{ asset('assets/plugins/datatables/responsive.bootstrap.min.js') }}"></script>
-    <script>
+<!-- DATATABLES & DEPENDENCIES -->
+<script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.13.6/js/dataTables.bootstrap5.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/2.4.1/js/dataTables.buttons.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/2.4.1/js/buttons.colVis.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/2.4.1/js/buttons.html5.min.js"></script>
+
+<!-- LOASH DEBOUNCE (untuk search optimized) -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.21/lodash.min.js"></script>
+
+<script>
         function fetchDataDropdown(url, id, placeholder, name, callback) {
             DataManager.executeOperations(url, 'admin_' + url, 120).then(response => {
                 $(id).empty().append('<option></option>');
@@ -106,8 +105,4 @@
         }
 
     </script>
-    @include('admin.sdm.script.list')
-    @include('admin.sdm.script.create')
-    @include('admin.sdm.script.edit')
-    @include('admin.sdm.script.detail')
 @endsection
--- resources/views/admin/sdm/script/list.blade.php
+++ resources/views/admin/sdm/script/list.blade.php
@@ -1,99 +1,100 @@
 <script>
-    function load_sdm() {
-        $.fn.dataTable.ext.errMode = 'none';
-        const table = $('#example').DataTable({
-            dom: 'lBfrtip',
-            stateSave: true,
-            stateDuration: -1,
-            pageLength: 10,
-            lengthMenu: [
-                [10, 15, 20, 25],
-                [10, 15, 20, 25]
-            ],
-            buttons: [
-                {
-                    extend: 'colvis',
-                    collectionLayout: 'fixed columns',
-                    collectionTitle: 'Pilih Kolom',
-                    className: 'btn btn-sm btn-dark rounded-2',
-                    columns: ':not(.noVis)'
-                },
-                {
-                    extend: 'csv',
-                    titleAttr: 'Export ke CSV',
-                    className: 'btn btn-sm btn-dark rounded-2',
-                },
-                {
-                    extend: 'excel',
-                    titleAttr: 'Export ke Excel',
-                    className: 'btn btn-sm btn-dark rounded-2',
-                },
-            ],
-            processing: true,
-            serverSide: true,
-            responsive: true,
-            searchHighlight: true,
-            ajax: {
-                url: '{{ route('admin.sdm.list') }}', // ambil data SDM dari controller
-                cache: false,
+function load_sdm() {
+    $.fn.dataTable.ext.errMode = 'none';
+    const table = $('#sdmTable').DataTable({
+        dom: 'lBfrtip',
+        stateSave: true,
+        stateDuration: -1,
+        pageLength: 10,
+        lengthMenu: [
+            [10, 15, 20, 25],
+            [10, 15, 20, 25]
+        ],
+        buttons: [
+            {
+                extend: 'colvis',
+                collectionLayout: 'fixed columns',
+                collectionTitle: 'Pilih Kolom',
+                className: 'btn btn-sm btn-dark rounded-2',
+                columns: ':not(.noVis)'
             },
-            order: [],
-            ordering: true,
-            columns: [
-                {
-                    data: 'action',
-                    name: 'action',
-                    orderable: false,
-                    searchable: false
-                },
-                {
-                    data: 'nip',
-                    name: 'nip'
-                },
-                {
-                    data: 'status_pegawai',
-                    name: 'status_pegawai'
-                },
-                {
-                    data: 'tipe_pegawai',
-                    name: 'tipe_pegawai'
-                },
-                {
-                    data: 'tanggal_masuk',
-                    name: 'tanggal_masuk',
-                    render: function (data) {
-                        return data ? new Date(data).toLocaleDateString('id-ID') : '-';
-                    }
-                },
-                {
-                    data: 'id_person',
-                    name: 'id_person'
-                },
-                {
-                    data: 'nama_lengkap',
-                    name: 'nama_lengkap'
-                },
-            ],
-        });
-
-        // search optimized biar nggak berat
-        const performOptimizedSearch = _.debounce(function (query) {
-            try {
-                if (query.length >= 3 || query.length === 0) {
-                    table.search(query).draw();
+            {
+                extend: 'csv',
+                titleAttr: 'Export ke CSV',
+                className: 'btn btn-sm btn-dark rounded-2',
+            },
+            {
+                extend: 'excel',
+                titleAttr: 'Export ke Excel',
+                className: 'btn btn-sm btn-dark rounded-2',
+            },
+        ],
+        processing: true,
+        serverSide: true,
+        responsive: true,
+        searchHighlight: true,
+        ajax: {
+            url: '{{ route('admin.sdm.list') }}',
+            cache: false,
+        },
+        order: [],
+        ordering: true,
+        columns: [
+            {
+                data: 'action',
+                name: 'action',
+                orderable: false,
+                searchable: false
+            },
+            {
+                data: 'nip',
+                name: 'nip'
+            },
+            {
+                data: 'status_pegawai',
+                name: 'status_pegawai'
+            },
+            {
+                data: 'tipe_pegawai',
+                name: 'tipe_pegawai'
+            },
+            {
+                data: 'tanggal_masuk',
+                name: 'tanggal_masuk',
+                render: function (data) {
+                    return data ? new Date(data).toLocaleDateString('id-ID') : '-';
                 }
-            } catch (error) {
-                console.error('Error during search:', error);
-            }
-        }, 800);
-
-        $('#example_filter input').unbind().on('input', function () {
-            performOptimizedSearch($(this).val());
-        });
-    }
-
-    // panggil fungsi saat halaman siap
-    $(document).ready(function () {
-        load_sdm();
+            },
+            {
+                data: 'id_person',
+                name: 'id_person'
+            },
+            {
+                data: 'nama_lengkap',
+                name: 'nama_lengkap'
+            },
+        ],
     });
+
+    // PERBAIKI SELECTOR UNTUK SEARCH INPUT
+    const performOptimizedSearch = _.debounce(function (query) {
+        try {
+            if (query.length >= 3 || query.length === 0) {
+                table.search(query).draw();
+            }
+        } catch (error) {
+            console.error('Error during search:', error);
+        }
+    }, 800);
+
+    // Ganti '#example_filter' dengan selector yang tepat
+    $('#sdmTable_filter input').unbind().on('input', function () {
+        performOptimizedSearch($(this).val());
+    });
+}
+
+// panggil fungsi saat halaman siap
+$(document).ready(function () {
+    load_sdm();
+});
 </script>
